5237db52682dd484d9b0bf883ec0de40e28570ad
Christelle Nieves <christellejnieves@gmail.com>
2021-02-17 18:07:53 -0500

    added messsages table to database

--- setup_db.py
+++ setup_db.py
@@ -9,12 +9,22 @@ cur = con.cursor()
 # Drop the user table if it exists
 cur.execute('DROP TABLE IF EXISTS User')
 
+# Drop the messages table if it exists
+cur.execute('DROP TABLE IF EXISTS Messages')
+
 con.commit()
 
 # Create the User table
 cur.execute('''CREATE TABLE User(
     User_id INTEGER PRIMARY KEY AUTOINCREMENT,
     Username TEXT NOT NULL)
+    ''')
+
+# Create the Messages table
+cur.execute('''CREATE TABLE Messages(
+    Message_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Username TEXT NOT NULL,
+    Content TEXT)
     ''')
 
 con.commit()
